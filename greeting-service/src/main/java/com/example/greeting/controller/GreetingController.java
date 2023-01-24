9f1540b02e19b012209a4b6919d9ce1b2fecb2ab
Suraj Sharma <suraj.sharma@pareteum.com>
2023-01-24 12:01:13 +0530

    Changes to add direct export to prometheus and zipkin

--- greeting-service/src/main/java/com/example/greeting/controller/GreetingController.java
+++ greeting-service/src/main/java/com/example/greeting/controller/GreetingController.java
@@ -7,14 +7,7 @@ import com.example.greeting.model.GreetingServiceGrpc.GreetingServiceImplBase;
 
 import net.devh.boot.grpc.server.service.GrpcService;
 
-import java.util.HashMap;
-
-import javax.annotation.PostConstruct;
-
 import io.grpc.stub.StreamObserver;
-import io.opentelemetry.api.GlobalOpenTelemetry;
-import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.Meter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -28,22 +21,9 @@ public class GreetingController extends GreetingServiceImplBase {
 			"Have a fruitful day"
 	};
 
-	private final Meter meter = GlobalOpenTelemetry.meterBuilder("io.opentelemetry.metrics.greeting").build();
-
-	private final HashMap<String, LongCounter> meterMap = new HashMap<>();
-
-	@PostConstruct
-	public void initCounter () {
-		for (String greeting : greetings) {
-			greeting = greeting.replaceAll(" ", "_");
-			meterMap.put(greeting, meter.counterBuilder(greeting).setUnit("int").build());
-		}
-	}
-
 	@Override
 	public void greet (GreetingRequest request, StreamObserver<GreetingResponse> responseObserver) {
 		final String greeting = greetings[request.getNum()];
-		meterMap.get(greeting).add(1);
 		final GreetingResponse greetingResponse = GreetingResponse.newBuilder().setGreeting(greeting).build();
 		responseObserver.onNext(greetingResponse);
 		responseObserver.onCompleted();
